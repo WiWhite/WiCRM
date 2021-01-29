2490120a26b5f06d459d3a648e70ce3f659b2ff2
Alexandr <bigsplash1990@gmail.com>
2021-01-29 21:38:20 +0200

    add search for CustomersList

--- WiCRM/customers/views.py
+++ WiCRM/customers/views.py
@@ -6,7 +6,16 @@ from .models import *
 class CustomersList(ListView):
     model = Customers
     template_name = 'customers/customers_list.html'
+    paginate_by = 12
 
     def get_queryset(self):
+        search = self.request.GET.get('search', '')
+
+        if search:
+            object_list = Customers.objects.filter(
+                company=search
+            ).select_related()
+            return object_list
+
         object_list = Customers.objects.all().select_related()
         return object_list
